356fdd354de19fc8dad9fac301beb0fcac252016
Aanchal Singh <a.singh.external@sportradar.com>
2025-05-24 00:30:47 +0530

    Initial setup

---
+++ src/main/java/com/ecommerce/springboot_ecommerce_api/SpringbootEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springboot_ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEcommerceApiApplication.class, args);
+	}
+
+}
